7d3fcbe1494ba75cf681147063f4f1fc4e9eb6e2
DuyQuang123456 <trieutrung951@gmail.com>
2023-04-19 17:55:10 +0700

    code js cart and device masterPage

--- src/main/java/fa/training/moocprojectfafull/controller/ControllerTest.java
+++ src/main/java/fa/training/moocprojectfafull/controller/ControllerTest.java
@@ -25,6 +25,7 @@ public class ControllerTest {
         model.addAttribute("product", Boolean.FALSE);
         model.addAttribute("contact", Boolean.FALSE);
         model.addAttribute("pageTitle", "Home");
+        model.addAttribute("direction","container/home");
         return "index";
     }
 
@@ -35,7 +36,8 @@ public class ControllerTest {
         model.addAttribute("product", Boolean.FALSE);
         model.addAttribute("contact", Boolean.FALSE);
         model.addAttribute("pageTitle", "Shop");
-        return "shop";
+        model.addAttribute("direction","container/shop");
+        return "index";
     }
 
     @GetMapping(value = "/product")
@@ -45,19 +47,22 @@ public class ControllerTest {
         model.addAttribute("product", Boolean.TRUE); // Active Product Nav Bar
         model.addAttribute("contact", Boolean.FALSE);
         model.addAttribute("pageTitle", "Product");
-        return "product";
+        model.addAttribute("direction","container/product");
+        return "index";
     }
 
     @GetMapping(value = "/cart")
     public String cart(Model model) {
         model.addAttribute("pageTitle", "Cart");
-        return "cart";
+        model.addAttribute("direction","container/cart");
+        return "index";
     }
 
     @GetMapping(value = "/checkout")
     public String checkOut(Model model) {
         model.addAttribute("pageTitle", "Check Out");
-        return "checkout";
+        model.addAttribute("direction","container/checkout");
+        return "index";
     }
 
     @GetMapping(value = "/contact")
@@ -67,6 +72,7 @@ public class ControllerTest {
         model.addAttribute("product", Boolean.FALSE);
         model.addAttribute("contact", Boolean.TRUE); // Active Contact Nav Bar
         model.addAttribute("pageTitle", "Contact");
-        return "contact";
+        model.addAttribute("direction","container/contact");
+        return "index";
     }
 }
